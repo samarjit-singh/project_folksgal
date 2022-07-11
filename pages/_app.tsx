17ff8e5d14296d815d18a93b72d1f4a2e4291521
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-07-12 02:01:27 +0530

    created components section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  // SSR - SERVER SIDE RENDERING
+  const [isSSR, setIsSSR] = useState(true);
+
+  // defining when the page is not used for server side rendering
+  useEffect(() => {
+    setIsSSR(false);
+  }, []);
+
+  if (isSSR) return null;
+
+  return (
+    <div>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyApp;
