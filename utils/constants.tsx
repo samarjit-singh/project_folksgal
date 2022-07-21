467cb32ecde3fd0f56593889a5d11bc7e48d6db0
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-07-22 04:26:44 +0530

    completed user profile component also

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,5 +1,5 @@
 import { BsCode, BsEmojiSunglasses } from "react-icons/bs";
-import { GiCakeSlice, GiGalaxy, GiLipstick } from "react-icons/gi";
+import { GiMountains, GiCakeSlice, GiGalaxy, GiLipstick } from "react-icons/gi";
 import { FaPaw, FaMedal, FaGamepad } from "react-icons/fa";
 
 export const topics = [
